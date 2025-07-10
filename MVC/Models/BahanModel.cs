37e26c70157d8324e962e1678fdff30983fa5926
IlhamWakana <113600726+IlhamWakana@users.noreply.github.com>
2025-07-10 22:19:13 +0700

    Add files via upload

---
+++ MVC/Models/BahanModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organresipi.MVC.Models
+{
+        public class BahanModel
+        {
+            public int Id { get; set; }
+            public string NamaBahan { get; set; }
+            public string Jumlah { get; set; }
+        }
+
+}
